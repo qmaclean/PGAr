8ff2895928bd53e8cde40fe02ddff647e7436bf2
Quinn MacLean <qmaclean@Quinns-MacBook-Pro.local>
2022-05-16 19:09:24 -0700

    pulls tourney stats leaders by stat category

--- fun/get_stats_leaderboards.R
+++ fun/get_stats_leaderboards.R
@@ -1,11 +1,16 @@
 
 
-year<-2021
+#year<-2021
 
 require(rjson)
 require(tidyverse)
 require(tidyjson)
 require(jsonlite)
+require(xml2)
+require(rvest)
+require(httr)
+require(janitor)
+
 
 
 #### to do figure out how to loop through week or tournament by tournament
@@ -17,14 +22,8 @@ require(jsonlite)
 #### stat IDs - to description
 
 ######### Overview ##################
-### 02671 - FedEx Cup Points
-### 138 - Top 10 Finishes
-### 120 - Scoring Avg
 
-### 02674 - Strokes Gained: Tee-To-Green
-### 02675 - Strokes Gained: Total
-### 02564 - Strokes Gained: Putting
-### 130 - Scrambling
+
 
 ######### Off The Tee ##################
 #### Strokes Gained ####
@@ -157,38 +156,490 @@ require(jsonlite)
 ### 360 - Birdie or Better Percentage - 125-150 Yds
 ### 361 - Birdie or Better Percentage - < 125 Yds
 ### 02379 - Approach > 275 Yds (RTP)
+### 02378 - Approach 250-275 Yds (RTP)
+### 02377 - Approach 225-250 Yds (RTP)
+### 02376 - Approach 200-225 Yds (RTP)
+### 480 - Approach >200 Yds (RTP Score)
+### 479 - Approach 175-200 Yds (RTP Score)
+### 478 - Approach 150-175 Yds (RTP Score)
+### 473 - Approach 125-150 Yds (RTP Score)
+### 472 - Approach <125 Yds (RTP Score)
+### 028 - Approach 100-125 Yds (RTP Score)
+### 029 - Approach 75 - 100 Yds (RTP Score)
+### 030 - Approach 50 - 75 Yds (RTP Score)
+### 02380 - Approaches 50-75 RGH (RTP)
+### 02381 - Approaches 75-100 RGH (RTP)
+### 02382 - Approaches 100-125 RGH (RTP)
+### 02383 - Approaches 50-125 RGH (RTP)
+### 02384 - Approaches 125-150 RGH (RTP)
+### 02385 - Approaches 150-175 RGH (RTP)
+### 02386 - Approaches 175-200 RGH (RTP)
+### 02387 - Approaches >200 RGH (RTP)
+### 02388 - Approaches < 100 Yds RGH (RTP)
+### 02389 - Approaches > 100 Yds RGH (RTP)
+### 02390 - Approaches 200-225 Yds RGH (RTP)
+### 02391 - Approaches 225-250 Yds RGH (RTP)
+### 02392 - Approaches 250-275 Yds RGH (RTP)
+### 02393 - Approaches >275 Yds RGH (RTP)
+### 469 Approaches Left Rough (RTP)
+### 470 Approaches Right Rough (RTP)
+### 471 Fairway Approach (RTP Score)
 
-### 104 - Putting Avg
-### 105 - NA
-### 106 - Total Eagles
-### 107 - Total Birdies
-### 108 - Scoring Avg (Actual)
-### 109 - Official Money Leaders
+#### Going For Green ####
+### 419 - Going for the Green
+### 486 - Going for the Green - Hit Green Pct
+### 02357 - Going for the Green - Birdie or Better
+### 436 - Par 5 Going for the Green
+### 02426 - Average Going For It Shot Distance (In Yards)
+### 02431 - Average Distance After Going For It Shot
+
+#### Holeouts, Other ####
+### 350 - Total Holeouts
+### 351 - Longest Holeouts (In Yds)
+### 02325 - Average Approach Shot Distance
+### 02338 - Average Approach Distance - Birdie or Better
+### 02339 - Average Approach Distance - Par
+### 02340 - Average Approach Distance - Bogey or Worse
+### 02430 - Average Distance to Hole After Tee Shot
+
+
+####### APPROACH THE GREEN ##########
+### 02569 - Strokes Gained: Around-The-Green
+
+#### UP AND DOWN ####
 ### 111 - Sand Save Pct
-### 331 - Proximity To Hole
+### 370 - Sand Saves From 30+ Yds
+### 371 - Sand Saves From 20-30 Yds
+### 372 - Sand Saves From 10-20 Yds
+### 373 - Sand Saves From <10 Yds
+### 130 - Scrambling
+### 362 - Scrambling From The Sand
+### 363 - Scrambling From The Rough
+### 364 - Scrambling From The Fringe
+### 365 - Scrambling From Other Locations
+### 366 - Scrambling From >30 Yds
+### 367 - Scrambling From 20-30 Yds
+### 368 - Scrambling From 10-20 Yds
+### 369 - Scrambling From < 10 Yds
+
+#### Accuracy ###
+### 374 - Proximity to Hole (ARG)
+### 375 - Proximity to Hole From Sand
+### 376 - Proximity to Hole From Rough
+### 377 - Proximity to Hole From Fringe
+### 378 - Proximity to Hole From Other Locations
+### 379 - Proximity to Hole From 30+ Yards
+### 380 - Proximity to Hole From 20-30 Yards
+### 381 - Proximity to Hole From 10-20 Yards
+### 382 - Proximity to Hole < 10 Yards
+### 481 - Scrambling Average Distance to Hole
+
+#### Scoring ####
+### 466 - Scrambling > 30 Yds (RTP Score)
+### 467 - Scrambling 20-30 Yds (RTP Score)
+### 468 - Scrambling 10-20 Yds (RTP Score)
+### 465 - Scrambling Fringe (RTP Score)
+### 464 - Scrambling Rough (RTP Score)
+
+
+####### PUTTING ##########
+### 02564 - Strokes Gained: Putting
+
+#### ONE-PUTTS ####
+### 413 - One-Putt Percentage
+### 414 - One-Putt Percentage - Round 1
+### 415 - One-Putt Percentage - Round 2
+### 416 - One-Putt Percentage - Round 3
+### 417 - One-Putt Percentage - Round 4
+### 418 - One-Putt Percentage - Round 5
+### 420 - Total 1 Putts - Inside 5'
+### 421 - Total 1 Putts - 5-10'
+### 422 - Total 1 Putts - 10-15'
+### 423 - Total 1 Putts - 15-20'
+### 424 - Total 1 Putts - 20-25'
+### 425 - Total 1 Putts - >25'
+### 398 - 1-Putts Per Round
+### 498 - Longest Putts
+
+#### THREE-PUTTS ####
+### 426 - 3-Putt Avoidance
+### 427 - 3-Putt Avoidance - Round 1
+### 428 - 3-Putt Avoidance - Round 2
+### 429 - 3-Putt Avoidance - Round 3
+### 430 - 3-Putt Avoidance - Round 4
+### 440 - 3-Putt Avoidance - Round 5
+### 068 - 3-Putt Avoidance - Inside 5'
+### 069 - 3-Putt Avoidance - 5-10'
+### 070 - 3-Putt Avoidance - 10-15'
+### 145 - 3-Putt Avoidance - 15-20'
+### 146 - 3-Putt Avoidance - 20-25'
+### 147 - 3-Putt Avoidance - >25'
+### 441 - Total 3 Putts - Inside 5'
+### 442 - Total 3 Putts - 5-10'
+### 443 - Total 3 Putts - 10-15'
+### 444 - Total 3 Putts - 15-20'
+### 445 - Total 3 Putts - 20-25'
+### 446 - Total 3 Putts - >25'
+### 400 - 3-Putts Per Round
+### 401 - 3+ Putts Per Round
+
+#### ALL PUTTS MADE BY DIST. ####
+### 408 - Putting From > 25'
+### 02429 - Putting From > 20'
+### 02328 - Putting From 15-25'
+### 407 - Putting From 20-25'
+### 406 - Putting From 15-20'
+### 02327 - Putting From 5-15'
+### 405 - Putting From 10-15'
+### 484 - Putting From Inside 10'
+### 404 - Putting From 5-10'
+### 02427 - Putting From 3-5'
+### 403 - Putting From Inside 5'
+### 348 - Putting From 10'
+### 347 - Putting From 9'
+### 346 - Putting From 8'
+### 345 - Putting From 7'
+### 344 - Putting From 6'
+### 343 - Putting From 5'
+### 356 - Putting From > 10'
+### 485 - Putting From 4-8'
+### 342 - Putting From 4'
+### 341 - Putting From 3'
+### 434 - Putts Made Per Event Over 10'
+### 435 - Putts Made Per Event Over 20'
+
+#### GIR PUTTS MADE BY DIST. ####
+### 073 - GIR Putting Avg > 35'
+### 072 - GIR Putting Avg - 30-35'
+### 071 - GIR Putting Avg - 25-30'
+### 388 - GIR Putting > 25'
+### 387 - GIR Putting 20-25'
+### 386 - GIR Putting 15-20'
+### 385 - GIR Putting 10-15'
+### 384 - GIR Putting 5-10'
+### 383 - GIR Putting - Inside 5'
+
+#### PUTTS PER ROUND ####
+### 119 - Putts Per Round
+### 393 - Putts Per Round - Round 1
+### 394 - Putts Per Round - Round 2
+### 395 - Putts Per Round - Round 3
+### 396 - Putts Per Round - Round 4
+### 397 - Putts Per Round - Round 5
+### 398 - 1-Putts Per Round
+### 399 - 2-Putts Per Round
+### 400 - 3-Putts Per Round
+### 401 - 3+ Putts Per Round
+
+#### PUTTING AVERAGES ####
+### 02428 - Total Putting
+### 02439 - Bonus Putting
+### 104 - Putting Avg
+### 402 - Overall Putting Average
+### 115 - Birdie or Better Conversion Percentage
+
+#### Avg. Putting Dist. ####
+### 409 - Average Putting Distance - ALL 1 Putts
+### 410 - Average Putting Distnace - ALL 2 Putts
+### 411 - Average Putting Distance - ALL 3 Putts
+### 412 - Average Putting Distance - ALL 3+ Putts
+### 389 - Average Putting Distance - GIR 1 Putts
+### 390 - Average Putting Distance - GIR 2 Putts
+### 391 - Average Putting Distance - GIR 3 Putts
+### 392 - Average Putting Distance - GIR 3+ Putts
+### 438 - Average Distance of Putts Made
+### 02440 - Average Distance of Birdie Putts Made
+### 02442 - Average Distance of Eagle Putts Made
+### 135 - Putts Made Distance
+### 349 - Approach Putt Performance
+
+
+####### SCORING ##########
+#### Strokes Gained ####
+### 02675 - Strokes Gained: Total
+
+#### Scoring Overall ####
+### 120 - Scoring Avg
+### 108 - Scoring Avg (Actual)
+### 116 - Scoring Avg Before Cut
+### 02417 - Stroke Differential Field Average
+### 299 - Lowest Round
+### 152 - Rounds in the 60s
+### 153 - Sub-Par Rounds
+
+#### Under Par Scoring ####
+### 112 - Par 3 Birdie or Better Leaders
+### 113 - Par 4 Birdie or Better Leaders
+### 114 - Par 5 Birdie or Better Leaders
+### 447 - Par 4 Eagle Leaders
+### 448 - Par 5 Eagle Leaders
+### 155 - Eagles (Holes Per)
+### 106 - Total Eagles
+### 105 - Par Breakers
+### 156 - Birdie Average
+### 107 - Total Birdies
+### 160 - Bounce Back
+### 02415 - Birdie to Bogey Ratio
+### 352 - Birdie or Better Percentage
+### 357 - Birdie or Better Percentage - 200+ Yds
+### 358 - Birdie or Better Percentage - 175 - 200 Yds
+### 359 - Birdie or Better Percentage - 150-175 Yds
+### 360 - Birdie or Better Percentage - 125 - 150 Yds
+### 361 - Birdie or Better Percentage - <125 Yds
+### 02333 - Birdie or Better Percentage - Fairway
+### 02334 - Birdie or Better Percentage - Left Rough
+### 02335 - Birdie or Better Percentage - Right Rough
+### 02336 - Birdie or Better Percentage - Rough
+
+#### Over Par Scoring ####
+### 02414 - Bogey Avoidance
+### 02415 - Birdie to Bogey Ratio
+### 02416 - Reverse Bounce Back
+### 02419 - Bogey Average
+
+#### Scoring By Round ####
+### 118 - Final Round Scoring Average
+### 219 - Final Round Performance
+### 220 - Top 10 Final Round Performance
+### 309 - Top 5 Final Round Performance
+### 310 - 11-25 Final Round Performance
+### 311 - 25+ Final Round Performance
+### 453 - 6 - 10 Final Round Performance
+### 148 - Round 1 Scoring Average
+### 149 - Round 2 Scoring Average
+### 117 - Round 3 Scoring Average
+### 285 - Round 4 Scoring Average
+### 286 - Round 5 Scoring Average
+### 245 - Front 9 Round 1 Scoring Average
+### 246 - Back 9 Round 1 Scoring Average
+### 253 - Front 9 Round 2 Scoring Average
+### 254 - Back 9 Round 2 Scoring Average
+### 261 - Front 9 Round 3 Scoring Average
+#### missing back 9 round 3 scoring average
+### 269 - Front 9 Round 4 Scoring Average
+### 277 - Front 9 Round 5 Scoring Average
+
+#### Par 3,4,5 Scoring ####
+### 171 - Par 3 Performance
+### 142 - Par 3 Scoring Average
+### 112 - Par 3 Birdie or Better Leaders
+### 172 - Par 4 Performance
+### 143 - Par 4 Scoring Average
+### 113 - Par 4 Birdie or Better Leaders
+### 173 - Par 5 Performance
+### 144 - Par 5 Scoring Average
+### 448 - Par 5 Eagle Leaders
+### 114 - Par 5 Birdie or Better Leaders
+### 223 - Early Par 3 Scoring Average
+### 231 - Early Par 4 Scoring Average
+### 239 - Early Par 5 Scoring Average
+### 224 - Late Par 3 Scoring Average
+### 232 - Late Par 4 Scoring Average
+### 240 - Late Par 5 Scoring Average
+
+#### Front 9 Scoring ####
+### 207 - Front 9 Scoring Average
+### 301 - Front 9 Lowest Round
+### 221 - Front 9 Par 3 Scoring Average
+### 229 - Front 9 Par 4 Scoring Average
+### 237 - Front 9 Par 5 Scoring Average
+### 245 - Front 9 Round 1 Scoring Average
+##@ 253 - Front 9 Round 2 Scoring Average
+### 261 - Front 9 Round 3 Scoring Average
+### 269 - Front 9 Round 4 Scoring Average
+### 277 - Front 9 Round 5 Scoring Average
+
+#### Back 9 Scoring ####
+### 208 - Back 9 Scoring Average
+### 302 - Back 9 Lowest Round
+### 222 - Back 9 Par 3 Scoring Average
+### 230 - Back 9 Par 4 Scoring Average
+### 238 - Back 9 Par 5 Scoring Average
+### 246 - Back 9 Round 1 Scoring Average
+### 254 - Back 9 Round 2 Scoring Average
+### 262 - Back 9 Round 3 Scoring Average
+### 270 - Back 9 Round 4 Scoring Average
+### 278 - Back 9 Round 5 Scoring Average
+
+#### Early Scoring ####
+### 292 - Early Scoring Average
+### 303 - Early Lowest Round
+### 209 - First Tee Early Scoring Average
+### 210 - Tenth Tee Early Scoring Average
+### 223 - Early Par 3 Scoring Average
+### 231 - Early Par 4 Scoring Average
+### 239 - Early Par 5 Scoring Average
+### 247 - Early Round 1 Scoring Average
+### 255 - Early Round 2 Scoring Average
+### 263 - Early Round 3 Scoring Average
+### 271 - Early Round 4 Scoring Average
+### 279 - Early Round 5 Scoring Average
+### 225 - First Tee Early Par 3 Scoring Average
+### 226 - Tenth Tee Early Par 3 Scoring Average
+### 233 - First Tee Early Par 4 Scoring Average
+### 234 - Tenth Tee Early Par 4 Scoring Average
+### 241 - First Tee Early Par 5 Scoring Average
+### 242 - Tenth Tee Early Par 5 Scoring Average
+### 249 - First Tee Early Round 1 Scoring Average
+### 250 - Tenth Tee Early Round 1 Scoring Average
+### 257 - First Tee Early Round 2 Scoring Average
+### 258 - Tenth Tee Early Round 2 Scoring Average
+### 265 - First Tee Early Round 3 Scoring Average
+### 266 - Tenth Tee Early Round 3 Scoring Average
+### 273 - First Tee Early Round 4 Scoring Average
+### 274 - Tenth Tee Early Round 4 Scoring Average
+### 281 - First Tee Early Round 5 Scoring Average
+### 282 - Tenth Tee Early Round 5 Scoring Average
+### 305 - First Tee Early Lowest Round
+### 306 - Tenth Tee Early Lowest Round
+
+
+#### Late Scoring ####
+### 293 - Late Scoring Average
+### 304 - Late Lowest Round
+### 224 - Late Par 3 Scoring Average
+### 223 - Late Par 4 Scoring Average
+### 240 - Late Par 5 Scoring Average
+### 248 - Late Round 1 Scoring Average
+### 256 - Late Round 2 Scoring Average
+### 264 - Late Round 3 Scoring Average
+### 272 - Late Round 4 Scoring Average
+### 280 - Late Round 5 Scoring Average
+### 211 - First Tee Late Scoring Average
+### 212 - Tenth Tee Late Scoring Average
+### 227 - First Tee Late Par 3 Scoring Average
+### 228 - Tenth Tee Late Par 3 Scoring Average
+### 235 - First Tee Late Par 4 Scoring Average
+### 236 - Tenth Tee Late Par 4 Scoring Average
+### 243 - First Tee Late Par 5 Scoring Average
+### 244 - Tenth Tee Late Par 5 Scoring Average
+### 251 - First Tee Late Round 1 Scoring Average
+### 252 - Tenth Tee Late Round 1 Scoring Average
+### 259 - First Tee Late Round 2 Scoring Average
+### 260 - Tenth Tee Late Round 2 Scoring Average
+### 267 - First Tee Late Round 3 Scoring Average
+### 268 - Tenth Tee Late Round 3 Scoring Average
+### 275 - First Tee Late Round 4 Scoring Average
+### 276 - Tenth Tee Late Round 4 Scoring Average
+### 283 - First Tee Late Round 5 Scoring Average
+### 284 - Tenth Tee Late Round 5 Scoring Average
+### 307 - First Tee Late Lowest Round
+### 308 - Tenth Tee Late Lowest Round
+
+#### Efficiency Scoring ####
+### 02517 - Par 3 Efficiency < 100 Yds
+### 02518 - Par 3 Efficiency 100-125 Yds
+### 02519 - Par 3 Efficiency 125-150 Yds
+### 02520 - Par 3 Efficiency 150-175 Yds
+### 02521 - Par 3 Efficiency 175-200 Yds
+### 02522 - Par 3 Efficiency 200-225 Yds
+### 02523 - Par 3 Efficiency 225-250 Yds
+### 02524 - Par 3 Efficiency >250 Yds
+### 02525 - Par 3 Efficiency <250 Yds
+### 02526 - Par 4 Efficiency 250-300 Yds
+### 02527 - Par 4 Efficiency 300-350 Yds
+### 02528 - Par 4 Efficiency 350-400 Yds
+### 02529 - Par 4 Efficiency 400-450 Yds
+### 02530 - Par 4 Efficiency 450-500 Yds
+### 02531 - Par 4 Efficiency >500 Yds
+### 02532 - Par 5 Efficiency <500 Yds
+### 02533 - Par 5 Efficiency 500-550 Yds
+### 02534 - Par 5 Efficiency 550-600 Yds
+### 02535 - Par 5 Efficiency 600-650 Yds
+### 02536 - Par 5 Efficiency >650 Yds
+
+
+######## Streaks ########
+
+#### Off The Tee Streaks ####
+### 297 - Consecutive Fairways Hit
+
+#### Approach the Green Streaks ####
+### 298 - Consecutive GIR 
+
+#### Around the Green Streaks ####
+### 296 - Consecutive Sand Saves 
+
+#### Putting Streaks ####
+### 294 - Best YTD Streak w/o a 3-Putt
+### 295 - Best YTD 1-Putt or Better Streak
+### 483 - Current Streak Without a 3-Putt
+
+#### Scoring Streaks ####
+### 474 - Best Rounds in 60's Streak
+### 475 - YTD Rounds in 60's Streak
+### 476 - Best Sub-Par Rounds Streaks
+### 477 - YTD Sub-Par Rounds Streak
+### 150 - Current Par or Better Streak
+### 482 - YTD Par or Better Streak
+### 449 - Consecutive Par 3 Birdies
+### 450 - Consecutive Par 4 Birdies
+### 451 - Consecutive Par 5 Birdies
+### 452 - Consecutive Holes Below Par
+### 02672 - Consecutive Birdies Streak
+### 02673 - Consecutive Birdies/Eagles Streak
+
+#### Other Streaks ####
+### 122 - Consecutive Cuts
+### 137 - YTD Consecutive Cuts
+### 438 - Current Streak w/o a 3-Putt
+
+
+######## Money/Finishes ########
+#### Money #####
+### 109 - Official Money Leaders
+### 110 - Career Money Leaders
+### 014 - Career Earnings
+### 139 - Non-Member Earnings
+### 01677 - Non-Member Off+WGC Earnings
+### 02444 - Money Leaders - Fall Series
+### 02396 - FedEx Cup Bonus Money
+### 154 - Money Per Event Leaders
+### 194 - Total Money (Official and Unofficial)
+### 02337 - Percentage of Available Purse Won
+### 02447 - Percentage of Potential Money Won
+
+#### Finishes ####
+### 138 - Top 10 Finishes
+### 300 - Victory Leaders
+
+
+######## Points/Rankings ########
+### 02671 - FedEx Cup Points
 
 
 
+get_tourney_stats_leaderboard<-function(year,
+                                          tournament_id,
+                                          stat_id) {
 
+### add precheck & required packages  
+  
 
-
-
-###### research -> as of tourney pull 
-
-base_url<-"https://statdata.pgatour.com/r/stats/"
-stat_id<-'02'
+base_url<-"https://www.pgatour.com/content/pgatour/stats/stat."
 
 full_url<-paste0(
-  base_url,year,
-  "/",stat_id,
-  ".json"
+  base_url,
+  ".",stat_id,
+  ".y",year,
+  ".eon.t",tournament_id,
+  ".html"
 )
 
-stat_js<-rjson::fromJSON(file=URLencode(full_url))
 
-df<-stat_js$tours[[1]]$years[[1]]$stats[[1]]$details %>%
-  spread_all() %>%
-  as_tibble()
+#full_url<-"https://www.pgatour.com/content/pgatour/stats/stat.02674.y2017.eon.t030.html"
+
+df<-read_html(full_url) %>%
+  html_nodes("body") %>%
+  html_table(fill=TRUE) %>%
+  as.data.frame() %>%
+  dplyr::filter(complete.cases(X4)) %>%
+  janitor::row_to_names(row_number = 1)
+
+return(df)
+
+}
+
 
 
 
