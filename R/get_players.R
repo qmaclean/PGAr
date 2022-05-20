1d8f2006a49b35fbd7fcc64c63cbbadaec94877b
Quinn MacLean <qmaclean@Quinns-MacBook-Pro.local>
2022-05-20 12:30:08 -0700

    add other tours

--- R/get_players.R
+++ R/get_players.R
@@ -1,7 +1,7 @@
 
 #year<-2022
 
-get_pga_players<-function(year) {
+get_pga_tour_players<-function(year) {
   
   require(jsonlite)
   require(tidyverse)
@@ -28,5 +28,124 @@ players<-players_js$plrs %>%
 return(df)
 
 }
+
+get_pga_champions_players<-function(year) {
+  
+  require(jsonlite)
+  require(tidyverse)
+  
+  
+  players_url<-"https://statdata.pgatour.com/players/player.json"
+  
+  res <- httr::RETRY("GET", players_url)
+  
+  resp<-res %>%
+    httr::content(as = "text",encoding = "UTF-8")
+  
+  players_js<-jsonlite::fromJSON(resp,flatten = TRUE)
+  
+  players<-players_js$plrs %>%
+    unnest(yrs) %>%
+    dplyr::filter(year == yrs,
+                  s == "y") %>%
+    select(-playedYrs,-displayYrs) %>%
+    as_tibble() 
+  
+  
+  
+  return(df)
+  
+}
+
+### korn ferry
+
+get_korn_ferry_players<-function(year) {
+  
+  require(jsonlite)
+  require(tidyverse)
+  
+  
+  players_url<-"https://statdata.pgatour.com/players/player.json"
+  
+  res <- httr::RETRY("GET", players_url)
+  
+  resp<-res %>%
+    httr::content(as = "text",encoding = "UTF-8")
+  
+  players_js<-jsonlite::fromJSON(resp,flatten = TRUE)
+  
+  players<-players_js$plrs %>%
+    unnest(yrs) %>%
+    dplyr::filter(year == yrs,
+                  h == "y") %>%
+    select(-playedYrs,-displayYrs) %>%
+    as_tibble() 
+  
+  
+  
+  return(df)
+  
+}
+
+
+####
+get_pga_latinoamerica_players<-function(year) {
+  
+  require(jsonlite)
+  require(tidyverse)
+  
+  
+  players_url<-"https://statdata.pgatour.com/players/player.json"
+  
+  res <- httr::RETRY("GET", players_url)
+  
+  resp<-res %>%
+    httr::content(as = "text",encoding = "UTF-8")
+  
+  players_js<-jsonlite::fromJSON(resp,flatten = TRUE)
+  
+  players<-players_js$plrs %>%
+    unnest(yrs) %>%
+    dplyr::filter(year == yrs,
+                  m == "y") %>%
+    select(-playedYrs,-displayYrs) %>%
+    as_tibble() 
+  
+  
+  
+  return(df)
+  
+}
+
+
+get_pga_canada_players<-function(year) {
+  
+  require(jsonlite)
+  require(tidyverse)
+  
+  
+  players_url<-"https://statdata.pgatour.com/players/player.json"
+  
+  res <- httr::RETRY("GET", players_url)
+  
+  resp<-res %>%
+    httr::content(as = "text",encoding = "UTF-8")
+  
+  players_js<-jsonlite::fromJSON(resp,flatten = TRUE)
+  
+  players<-players_js$plrs %>%
+    unnest(yrs) %>%
+    dplyr::filter(year == yrs,
+                  c == "y") %>%
+    select(-playedYrs,-displayYrs) %>%
+    as_tibble() 
+  
+  
+  
+  return(df)
+  
+}
+
+
 
 
